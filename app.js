a55f3b0c3753c2c47e08fb5a6d810bba8a9248d2
smirchan <smirchan@gmail.com>
2021-06-29 11:00:50 -0400

    Updated prototype

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get("/*", function(req, res) {
     // } else {
         // res.sendFile(__dirname + "/" + file);
     // }
-    res.sendFile(__dirname + "/" + file)
+    res.sendFile(__dirname + "/" + file);
 });
 
 // socket.io will call this function when a client connects
